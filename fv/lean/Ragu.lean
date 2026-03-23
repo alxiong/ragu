515f08b3743b89bc02bb96a56aed21676a0c1d51
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2026-03-23 15:48:57 +0100

    fv: reorder imports to make extraction checks pass

--- fv/lean/Ragu.lean
+++ fv/lean/Ragu.lean
@@ -1,4 +1,4 @@
 import Ragu.Instances.Point.AllocFp
 import Ragu.Instances.Point.AllocFq
-import Ragu.Instances.Point.AddIncomplete
 import Ragu.Instances.Point.Double
+import Ragu.Instances.Point.AddIncomplete
